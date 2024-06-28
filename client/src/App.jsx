d249c56994b21b40fd8ef0861fbad6d673250b22
Redoy <redoysaha3236@gmail.com>
2024-06-29 01:16:10 +0600

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import { ThemeProvider } from "./provider/provider";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./page/home";
+import Dashboard from "./page/dashboard";
+import SignUp from "./page/sign-up";
+import SignIn from "./page/sign-in";
 
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="App">App</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
     </ThemeProvider>
   );
 };
