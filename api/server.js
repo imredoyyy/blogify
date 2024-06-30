eb17e23d35bf0ef802badfecab8ca95ec7d683a4
Redoy <redoysaha3236@gmail.com>
2024-06-30 23:05:06 +0600

    connect to database

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { connectToDb } from "./utils/connect-to-db";
 
 const app = express();
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-  console.log("Listening on http://localhost:3000");
+  console.log(`Listening on http://localhost:${PORT}`);
 });
