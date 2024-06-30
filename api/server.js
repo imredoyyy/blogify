eacc0e1a8ceca5a562f0ac7dad079563babb287f
Redoy <redoysaha3236@gmail.com>
2024-06-30 23:55:58 +0600

    create test api route

--- api/server.js
+++ api/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import { connectToDb } from "./utils/connect-to-db";
 
-const app = express();
+import userRoute from "./routes/user.route";
 
+const app = express();
+const PORT = 3000;
 connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/user", userRoute);
+console.log("User route registered");
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
