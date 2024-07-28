2e93059738cf58d1e02a3d259d3a0f0b032b109d
Redoy <redoysaha3236@gmail.com>
2024-07-29 01:33:55 +0600

    add pre deployment

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { connectToDb } from "./utils/connect-to-db";
-
 import userRoute from "./routes/user.route";
 import authRoute from "./routes/auth.route";
 import postRoute from "./routes/post.route";
 import commentRoute from "./routes/comment.route";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -13,13 +16,18 @@ app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 await connectToDb();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-    .use("/api/user", userRoute)
-    .use("/api/auth", authRoute)
-    .use("/api/post", postRoute)
-    .use("/api/comment", commentRoute);
+const __dirname = path.resolve();
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
+app.use("/api/comment", commentRoute);
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use((err, req, res, next) => {
     if (err) {
